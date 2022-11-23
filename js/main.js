f5152878456d2495fd41d1399f7d4bf13da4823c
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-11-23 15:25:25 +0500

    initiol commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+var userName = prompt('Ismingizni Kiriting');
+var user_confirm = confirm('Testni Boshlaysizmi?');
+var css_test1 = prompt('Befor berilganda  element contentning qaysi tomonidan chiqadi?'+'\n'+'A-left'+'\n'+'B-right'+'\n'+'C-top'+'\n'+'D-button','Test1--');
+var user_test2 = prompt('Cllasni chaqirish uchun qanday belgi ishlatiladi?'+'\n'+'A - .'+'\n'+'B-#'+'\n'+'C-@'+'\n'+'D-$');
+var user_test3 = prompt('Box model nima?' + '\n'+'A-Contentning paddingi'+ '\n'+'B-Contentning margeni'+'\n'+'C-Contentning paddingi-margeni-borderi'+'\n'+'D-Barcha javoblar to\'g\'ri');
+
+
+var user_test4 = prompt('Htmlda chiziq chiqzrish uchun qaysi tegdan foydalaniladi?'+'\n'+'A-br'+'\n'+'B-a'+'\n'+'C-hr'+'\n'+'D-sup' );
+var user_test5 = prompt('Bottomning Nechta type bor?'+'\n'+'A-4'+'\n'+'B-3'+'\n'+'C-2'+'\n'+'D-type yo\'q');
+
+
+
+
+console.log(
+  
+    'Ismingiz'+ ' '+ userName
+);
+console.log(
+  
+    'Testni Boshlaysizmi?'+ ' '+ user_confirm
+);
+console.log(
+  
+    'Befor berilganda  element contentning qaysi tomonidan chiqadi?'+ '\n\t'+'Sizning Javobingiz -'+' '+ css_test1+'\n\t'+'To\'g\'ri Javob-'+' ' +'A'
+);
+console.log('Cllasni chaqirish uchun qanday belgi ishlatiladi?'+'\n\t'+'Sizning Javobingiz -'+' '+user_test2+'\n\t'+'To\'g\'ri Javob-'+' '+'A');
+
+console.log('Box model nima?'+'\n\t'+'Sizning Javobingiz -'+' '+user_test3+'\n\t'+'To\'g\'ri Javob-'+' '+'C');
+
+console.log('Htmlda chiziq chiqzrish uchun qaysi tegdan foydalaniladi?'+'\n\t'+'Sizning Javobingiz -'+' '+user_test4+'\n\t'+'To\'g\'ri Javob-'+' '+'C');
+
+console.log('Bottomning Nechta type bor?'+'\n\t'+'Sizning Javobingiz -'+' '+user_test5+'\n\t'+'To\'g\'ri Javob-'+' '+'B');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
++ '\n' + user_test + '\n' + css_test + '\n' + user_test
